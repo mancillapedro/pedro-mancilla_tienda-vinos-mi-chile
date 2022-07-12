c6d25b232bebc2d439475e49822ad0e2199b066c
_ <pedropmancillac@gmail.com>
2022-07-12 11:17:58 -0400

    avance home y bag

---
+++ src/components/BagComponent.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-card id="BagComponent">
+    <v-card-title v-text="`Bolsa de Compra Vinos Mi Chile`" />
+    <v-divider />
+    <v-card-text>
+      <v-card v-for="(product, i) in products" :key="i"></v-card>
+    </v-card-text>
+    <v-divider />
+    <v-card-text>
+      <v-row>
+        <template v-for="(total, i) in totales">
+          <v-col :key="i" v-text="total.text" />
+          <v-col :key="i" v-text="total.total" />
+        </template>
+      </v-row>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "BagComponent",
+  // props: {},
+  data: () => ({
+    products: [],
+  }),
+  computed: {
+    totales() {
+      return [
+        { text: "Subtotal Productos", total: 12 },
+        { text: "Descuentos", total: 23 },
+        { text: "Total", total: 34 },
+      ];
+    },
+  },
+  // methods: {},
+  // watch: {},
+  // components: {},
+  // mixins: [],
+  // filters: {},
+  // -- Lifecycle Methods
+  // -- End Lifecycle Methods
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,59 @@
 <template>
-  <div id="HomeView">
-    <h1 v-text="`HomeView`" />
-  </div>
+  <v-container id="HomeView">
+    <v-carousel
+      cycle
+      height="400"
+      hide-delimiter-background
+      show-arrows-on-hover
+    >
+      <v-carousel-item
+        v-for="(vino, i) in mejoresVinos"
+        :key="i"
+        :src="vino.imagen"
+      />
+    </v-carousel>
+    <h1 class="text-center" v-text="`Nuestros vinos`" />
+    <v-row>
+      <v-col cols="12" md="4" v-for="(vino, i) in tiposVino" :key="i">
+        <v-card>
+          <v-img height="250" :src="vino.imagen" />
+          <v-card-title v-text="vino.nombre" />
+          <v-card-text v-text="vino.texto" />
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
   name: "HomeView",
   // props: {},
-  // data: () => ({}),
+  data: () => ({
+    tiposVino: null,
+    mejoresVinos: null,
+  }),
   // computed: {},
-  // methods: {},
+  methods: {
+    async fetchHome() {
+      try {
+        const response = await fetch("/home.json");
+        if (!response.ok) throw "Error en API";
+        const json = await response.json();
+        this.tiposVino = json.tiposvino;
+        this.mejoresVinos = json.mejoresvinos;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
   // watch: {},
   // components: {},
   // mixins: [],
   // filters: {},
+  created() {
+    this.fetchHome();
+  },
   // -- Lifecycle Methods
   // -- End Lifecycle Methods
 };
