dee9061df3612f8b9dd63b8686e4e0f5c87424fa
_ <pedropmancillac@gmail.com>
2022-07-12 16:29:02 -0400

    navbar

---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav id="NavbarComponent">
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title v-text="`Vinos Mi Chile`" />
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        text
+        v-for="(btn, i) in btns"
+        :key="i"
+        v-text="btn.text"
+        @click="$router.push({ name: btn.route })"
+        :disabled="routeDisabled(btn.route)"
+      />
+    </v-app-bar>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavbarComponent",
+  // props: {},
+  data: () => ({
+    btns: [
+      { text: "Home", route: "home" },
+      { text: "Vinos", route: "vinos" },
+      { text: "Vinos en Promoción", route: "vinosEnPromocion" },
+      { text: "Accesorios", route: "accesorios" },
+    ],
+  }),
+  methods: {
+    routeDisabled(route){
+        return this.$route.name == route
+    }
+  },
+};
+</script>
