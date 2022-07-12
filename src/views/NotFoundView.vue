7eaf86bd6af7f6f2f0b43f16438b50e3aa8b5e7b
_ <pedropmancillac@gmail.com>
2022-07-12 15:08:28 -0400

    not found

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-container class="text-center" id="NotFoundView">
+    <h1 v-text="`Pagina no encontrada`" />
+    <router-link :to="{ name: 'home' }" v-text="`volver al home`" />
+  </v-container>
+</template>
+
+<script>
+export default { name: "NotFoundView" };
+</script>
